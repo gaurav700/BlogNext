0037ec13add7e097c5030bb5c0277ce62235aced
fg227 <fg227@njit.edu>
2025-02-10 19:54:42 +0000

    Using formaction state to show errors to the page after submitting the form

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,13 +1,30 @@
-import FormSubmit from '@/components/formSubmit';
+import FormActionSubmit from '@/components/formActionSubmit';
 import { storePost } from '@/lib/posts';
 import { redirect } from 'next/navigation';
 
 export default function NewPostPage() {
+  
   async function createPost(formData) {
     "use server";
     const title = formData.get('title');
     const image = formData.get('image');
     const content = formData.get('content');
+
+    let errors =[];
+    if(!title || title.trim().length === 0) {
+      errors.push('Title is required');
+    }
+    if(!image) {
+      errors.push('Image is required');
+    }
+    if(!content || content.trim().length === 0) {
+      errors.push('Content is required');
+    }
+
+    if(errors.length>0){
+      return {errors};
+    }
+
 
     await storePost({
       imageUrl: '',
@@ -19,33 +36,8 @@ export default function NewPostPage() {
     redirect('/feed');
   }
 
-
-  return (
-    <>
-      <h1>Create a new post</h1>
-      <form action={createPost}>
-        <p className="form-control">
-          <label htmlFor="title">Title</label>
-          <input type="text" id="title" name="title" required/>
-        </p>
-        <p className="form-control">
-          <label htmlFor="image">Image URL</label>
-          <input
-            type="file"
-            accept="image/png, image/jpeg"
-            id="image"
-            name="image"
-            required
-          />
-        </p>
-        <p className="form-control">
-          <label htmlFor="content">Content</label>
-          <textarea id="content" name="content" rows="5" required/>
-        </p>
-        <p className="form-actions">
-          <FormSubmit />
-        </p>
-      </form>
-    </>
-  );
+  return(
+    <FormActionSubmit />
+  )
+  
 }
